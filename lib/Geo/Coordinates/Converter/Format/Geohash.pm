66670c4792d3e39e7f04c044c24dd68349ab5d59
Kazuhiro Osawa <yappo+cpan@shibuya.pl>
2011-02-08 06:00:23 +0000

    Import of YAPPO/Geo-Coordinates-Converter-Format-Geohash-0.04 from CPAN.
    gitpan-cpan-distribution: Geo-Coordinates-Converter-Format-Geohash
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         YAPPO/Geo-Coordinates-Converter-Format-Geohash-0.04.tar.gz
    gitpan-cpan-author:       YAPPO
    gitpan-cpan-maturity:     released

--- lib/Geo/Coordinates/Converter/Format/Geohash.pm
+++ lib/Geo/Coordinates/Converter/Format/Geohash.pm
@@ -2,8 +2,9 @@ package Geo::Coordinates::Converter::Format::Geohash;
 use strict;
 use warnings;
 use parent 'Geo::Coordinates::Converter::Format';
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
+use Geo::Coordinates::Converter::Point::Geohash;
 use Geo::Hash;
 
 sub name { 'geohash' }
